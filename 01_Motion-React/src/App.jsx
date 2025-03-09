bee1c2de391d7655f5bed246124e970cc9d2353f
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-03-09 14:18:26 +0500

    Learning the basics of Framer Motion

---
+++ 01_Motion-React/src/App.jsx
@@ -0,0 +1,32 @@
+import { motion } from "framer-motion";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen w-screen bg-zinc-900 flex flex-col gap-20">
+        <motion.div
+          className="box w-40 h-40 m-10 bg-blue-500 border-2 border-white rounded-2xl"
+          animate={{ x: 1000, rotate: 180 }}
+          transition={{
+            duration: 2,
+            delay: 1,
+            ease: "easeInOut",
+            repeat: Infinity,
+          }}
+        ></motion.div>
+        <motion.div
+          className="box w-40 h-40 m-10 bg-green-500 border-2 border-white rounded-full"
+          animate={{ x: 800, y: 50, scale: 1.5 }}
+          transition={{
+            duration: 2,
+            delay: 1,
+            ease: "easeInOut",
+            repeat: Infinity,
+          }}
+        ></motion.div>
+      </div>
+    </>
+  );
+}
+
+export default App;
